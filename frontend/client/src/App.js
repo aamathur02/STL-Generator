87baeeb44058abb3e659eb531aa41f1aac3ef18c
Aayush Mathur <aamathur02@gmail.com>
2021-09-08 17:56:17 -0400

    some code cleanup

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
       <ImageUpload/>
       <ModelDownload/>
+      <ModelViewer/>
     </div>
   );
 }
--- frontend/client/src/components/ModelViewer.js
+++ frontend/client/src/components/ModelViewer.js
@@ -1,35 +1,15 @@
 import React, { Component } from "react";
+import STLViewer from 'stl-viewer'
 
-class ModelViewer extends Component {
-  constructor() {
-    super();
-    this.state = {
-        stlFile: null,
-        stlLink: null,
-    };
-    this._state = {
-        stlLink: window.location.href + 'bottle.stl',
-    }
-}
-  render() {
-    return (
-      <div className="container" id="div2">
-                    <label htmlFor="obj-file">
-                        Load STL by file
-                        <br/>
-                        <input type="file"
-                               name="obj-file"
-                               onChange={(e) => {
-                                   console.log(e.target.files)
-                                   this.setState({
-                                       stlFile: e.target.files[0]
-                                   })
-                                   console.log('path' + this.state.stlFile)
-                               }} placeholder="test"/>
-                    </label>
-                    
-                </div>
-    );
-  }
+const ModelViewer = () => {
+  <STLViewer
+	url='"http://127.0.0.1:5000/stl"'
+	width={400}
+	height={400}
+	modelColor='#B92C2C'
+	backgroundColor='#EAEAEA'
+	rotate={true}
+	orbitControls={true}
+/>
 }
 export default ModelViewer;
